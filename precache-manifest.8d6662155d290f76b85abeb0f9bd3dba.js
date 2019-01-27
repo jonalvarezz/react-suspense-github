9ddf5ca4d7ebd76466e96a675d5ee183dad34607
Jonathan Alvarez G <j@jonalvarezz.com>
2019-01-27 02:18:19 -0500

    Updates

---
+++ precache-manifest.8d6662155d290f76b85abeb0f9bd3dba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bc38838ac064c48eea0",
+    "url": "/react-suspense-github/static/js/runtime~main.4bc38838.js"
+  },
+  {
+    "revision": "6006f948781ae6904e7b",
+    "url": "/react-suspense-github/static/js/main.6006f948.chunk.js"
+  },
+  {
+    "revision": "54ddd9bf3069beab8372",
+    "url": "/react-suspense-github/static/js/4.54ddd9bf.chunk.js"
+  },
+  {
+    "revision": "8346cbf06fb72890b945",
+    "url": "/react-suspense-github/static/js/3.8346cbf0.chunk.js"
+  },
+  {
+    "revision": "5c7bef3b703578657502",
+    "url": "/react-suspense-github/static/js/2.5c7bef3b.chunk.js"
+  },
+  {
+    "revision": "97ad2ae1ac750251ea9a",
+    "url": "/react-suspense-github/static/js/1.97ad2ae1.chunk.js"
+  },
+  {
+    "revision": "8346cbf06fb72890b945",
+    "url": "/react-suspense-github/static/css/3.65dbb5d8.chunk.css"
+  },
+  {
+    "revision": "d368508de86338efdbfd4b3fd697fe8c",
+    "url": "/react-suspense-github/index.html"
+  }
+];
